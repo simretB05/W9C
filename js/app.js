4460e7c757624c33db4de29be6a014d8013b5871
simretB05 <happysimret@gmail.com>
2023-03-17 22:05:31 -0600

    created 3 arrays with list items and performed push and pop methods to access values, also added comments that describe what the code is doing

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// Defined three arrays to store information about a list of items
+let item_names = ["pen", "pencil", "marker"];
+let item_price =[ "$.50", "$.25", "$1"]
+let in_stock = [true, false, true];
+// Got the first item name and price from the arrays by accessing the first element usign the index number
+let first_item_name = item_names[0];
+let fist_item_price = item_price[0];
+// Add a new item, price, and availability to the end of the arrays using the push() method
+item_names.push( "note book" );
+item_price.push( "$4" );
+in_stock.push( true );
+// Removed the last item, price, and availability from each array using the pop() method and store their values in new variables
+let last_item_name = item_names.pop();
+let last_item_price = item_price.pop();
+let last_item_stock = in_stock.pop();
